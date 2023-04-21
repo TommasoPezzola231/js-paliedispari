b7128c5348c93ed8f43e97a94d04b2974bdf7ac2
TommasoPezzola <pezzola123@gmail.com>
2023-04-21 15:53:29 +0200

    Pari o Dispari

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/****************
+
 function parolaPalindroma(parolaUtente) {
 
     let = parolaAlContrario = "";
@@ -20,3 +22,31 @@ function parolaPalindroma(parolaUtente) {
 
 let parolaDaVerificare = parolaPalindroma(prompt("Inserisci una parola"));
 
+*********************************/
+
+function PariOrDispari(numeroUtente) {
+    
+    let numeroComputer = Math.floor(Math.random() * 5) + 1;
+
+    let numeroVerifica = numeroComputer + numeroUtente;
+
+    let risultato = "Ha vinto Dispari"
+
+    if (numeroVerifica % 2 == 0) {
+        risultato = "Ha vinto Pari!"
+    }
+
+    scelta = scelta.toLowerCase();
+
+    if ((scelta == "pari") && (risultato == "Ha vinto Pari!")) {
+        alert(`${numeroUtente} + ${numeroComputer} fa ${numeroVerifica} quindi Pari, congratulazioni hai vinto!`);
+    } else if ((scelta == "dispari") && (risultato == "Ha vinto Dispari")) {
+        alert(`${numeroUtente} + ${numeroComputer} fa ${numeroVerifica} quindi Dispari, congratulazioni hai vinto!`);
+    }  else {
+        alert(`${numeroUtente} + ${numeroComputer} fa ${numeroVerifica} quindi non è ${scelta} mi dispiace hai perso!`);
+    }
+}
+
+let scelta = prompt("Ti sfido a Pari o Dispari! Cosa scegli?");
+
+let numeroScelto = PariOrDispari(parseInt(prompt("Bene, scegli un numero tra 1 e 5")))
